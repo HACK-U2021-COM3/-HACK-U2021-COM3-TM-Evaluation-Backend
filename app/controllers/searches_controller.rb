d99d1706560cbea05220e3d07080a58efc467533
Densuke-fitness <densukefitness2020@gmail.com>
2021-09-05 04:10:43 +0900

    ruboCopのエラー判定の修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,8 @@ class SearchesController < ApplicationController
   def search
 
     begin
-      search_req_params = SearchReqParams.new(params) # rubocop:disable all
+      search_req_params = SearchReqParams.new(params)
     rescue => err
-      puts err.message
       resp = ErrorRespController.handle_status_code(err)
       render resp
       return
@@ -14,7 +13,6 @@ class SearchesController < ApplicationController
     begin
       got_places = SearchService.new(search_req_params).call_place_api
     rescue => err
-      puts err.message
       resp = ErrorRespController.handle_status_code(err)
       render resp
       return
