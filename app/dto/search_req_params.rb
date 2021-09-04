9b9ace23f4019bf34479ea74d0cd08630a02f8ef
Densuke-fitness <densukefitness2020@gmail.com>
2021-09-05 02:55:47 +0900

    #8 地点検索APIの実装

--- app/dto/search_req_params.rb
+++ app/dto/search_req_params.rb
@@ -3,6 +3,7 @@ class SearchReqParams < RequestDto
   validates :query_place, presence: true
 
   def initialize(params)
+    super
     @query_place = params[:query_place]
     #validateして異常があった場合
     raise SearchValidatorError.new unless valid?
