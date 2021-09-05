0507da57182e6fe1009172f3b84ea74e67af84aa
Densuke-fitness <densukefitness2020@gmail.com>
2021-09-05 13:10:30 +0900

    リファクタリング

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,23 +3,23 @@ class SearchesController < ApplicationController
   def search
 
     begin
-      search_req_params = SearchReqParams.new(params)
+      req_params = SearchReqParams.new(params)
     rescue => err
-      search_resp_params = ErrorRespController.handle_status_code(err)
-      render search_resp_params
+      resp_params = ErrorRespController.handle_status_code(err)
+      render resp_params
       return
     end
     
     begin
-      got_places = SearchService.new(search_req_params).call_place_api
+      got_places = SearchService.new(req_params).call_place_api
     rescue => err
-      search_resp_params = ErrorRespController.handle_status_code(err)
-      render 
+      resp_params = ErrorRespController.handle_status_code(err)
+      render resp_params
       return
     end
 
-    search_resp_params =  SuccessRespController.handle_status_code(got_places)
-    render search_resp_params
+    resp_params =  SuccessRespController.handle_status_code(got_places)
+    render resp_params
     return
 
   end
