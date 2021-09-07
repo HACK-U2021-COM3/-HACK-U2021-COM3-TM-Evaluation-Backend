0dd702cebe73d111de16998da75e7cf449801685
Yum-git <yzk.yuma@gmail.com>
2021-09-08 04:02:56 +0900

    Add 更新と削除の機能を追加しました

--- app/controllers/past_plans_controller.rb
+++ app/controllers/past_plans_controller.rb
@@ -67,9 +67,54 @@ class PastPlansController < ApplicationController
 
 
   def update
+    begin
+      plans_req_params = PastPlansReqParams.new(params)
+      id_req_params = PastPlanIdReqParams.new(user_id: @user_id, plans_id: params[:id].to_i)
+    rescue => err
+      resp_params = ErrorRespController.handle_status_code(err)
+      render resp_params
+      return
+    end
+
+    begin
+      past_plans_service_obj = PastPlansService.new({ plans_req_params: plans_req_params, id_req_params: id_req_params })
+      past_plans_service_obj.set_for_update
+      past_plans_service_obj.update_past_plan_and_detail
+    rescue => err
+      resp_params = ErrorRespController.handle_status_code(err)
+      render resp_params
+      return
+    end
+
+    resp_params = SuccessRespController.handle_status_code(SuccessRespController::SUCCESS_CODE_NO_CONTENT)
+    render resp_params
+    return
+
+
+
+
   end
 
 
+
+
+
+
   def destroy
+    req_params = PastPlanIdReqParams.new(user_id: @user_id, plans_id: params[:id].to_i)
+    begin
+      past_plans_service_obj = PastPlansService.new(req_params)
+      past_plans_service_obj.set_for_destroy
+      past_plans_service_obj.destroy_past_plan
+    rescue => err
+      resp_params = ErrorRespController.handle_status_code(err)
+      render resp_params
+      return
+    end
+
+    resp_params = SuccessRespController.handle_status_code(SuccessRespController::SUCCESS_CODE_NO_CONTENT)
+    render resp_params
+    return
+
   end
 end
