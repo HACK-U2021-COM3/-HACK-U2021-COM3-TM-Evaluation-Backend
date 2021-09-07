11a44de9ab4c82c5f6c59dab73c8b04b1b6f4e2f
Yum-git <yzk.yuma@gmail.com>
2021-09-07 22:11:18 +0900

    Fix ReqBodyを利用しているAPIについてGETからPOSTに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/search', to: 'searches#search'
 
-  get '/measure', to: 'measures#measure'
+  post '/measure', to: 'measures#measure'
 
   resources :past_plans, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
