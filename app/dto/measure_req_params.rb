de13ac530e16692d412d02fcc92e48294e1c3755
Yum-git <yzk.yuma@gmail.com>
2021-09-08 01:09:37 +0900

    Fix StayTimeを値に加える必要があるため修正しました

--- app/dto/measure_req_params.rb
+++ app/dto/measure_req_params.rb
@@ -1,6 +1,8 @@
 class MeasureReqParams < RequestDto
   attr_accessor :from, :to, :waypoints
   validates :from, :to, :waypoints, presence: true
+  validate  :from_contents_validation
+  validate  :to_contents_validation
   validate  :waypoints_contents_validation
   # rubocop:disable all
   def initialize(params={})
@@ -13,10 +15,26 @@ class MeasureReqParams < RequestDto
   end
   # rubocop:enable all
 
+  def from_contents_validation
+    from_contents =  @from
+
+    if from_contents[:from_name].blank?  || from_contents[:from_stay_time].blank?
+      errors.add(:from_contents, "from contents are empty!")
+    end
+  end
+
+  def to_contents_validation
+    to_contents =  @to
+
+    if to_contents[:to_name].blank?  || to_contents[:to_stay_time].blank?
+      errors.add(:to_contents, "to contents are empty!")
+    end
+  end
+
   def waypoints_contents_validation
     waypoints =  @waypoints
     waypoints.each do |waypoint|
-      if waypoint[:point].blank? || waypoint[:order].blank?
+      if waypoint[:point].blank? || waypoint[:point_stay_time].blank? || waypoint[:order].blank?
         errors.add(:waypoints, "waypoints contents are empty!")
       end
     end
